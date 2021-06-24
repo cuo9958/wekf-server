74bfeeb7e4c13c91516c3b38517ef4717deda6ce
guofangchao <guofangchao@daling.com>
2021-06-24 14:05:51 +0800

    add websocket

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import Koa from "koa";
 import KoaBody from "koa-body";
 import routers from "./router";
+import http from "http";
+import Socket from "socket.io";
 
 const app = new Koa();
+const server = http.createServer(app.callback());
+const SocketIO = new Socket.Server(server);
 
 app.use(KoaBody({}));
 
 //加载路由
 app.use(routers.routes()).use(routers.allowedMethods());
 
+//加载websocket
+SocketIO.on("connection", (client: Socket.Socket) => {
+    console.log("进入", client.id);
+});
+
 const port = process.env.PORT || "8082";
 
-app.listen(port, function () {
+server.listen(port, function () {
     console.log(`服务器运行在http://127.0.0.1:${port}`);
 });
