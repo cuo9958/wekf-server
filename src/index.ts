548cf8fde6e6b940f87a2cfc2d0956ee5459bc0b
guofangchao <guofangchao@daling.com>
2021-06-24 14:42:59 +0800

    demo

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,36 @@ import KoaBody from "koa-body";
 import routers from "./router";
 import http from "http";
 import Socket from "socket.io";
+import { UserJoin, UserLeave } from "./service/img";
 
 const app = new Koa();
 const server = http.createServer(app.callback());
-const SocketIO = new Socket.Server(server);
+const SocketIO = new Socket.Server(server, {
+    path: "/_img",
+    connectTimeout: 60000,
+});
 
-app.use(KoaBody({}));
+app.use(
+    KoaBody({
+        multipart: true,
+    })
+);
 
 //加载路由
 app.use(routers.routes()).use(routers.allowedMethods());
 
 //加载websocket
 SocketIO.on("connection", (client: Socket.Socket) => {
-    console.log("进入", client.id);
+    console.log("进入", client.id, client.handshake.query);
+    UserJoin(client.id);
+
+    client.on("event", (data) => {
+        console.log("事件", data);
+    });
+    client.on("disconnect", (msg) => {
+        console.log("用户退出", msg);
+        UserLeave(client.id);
+    });
 });
 
 const port = process.env.PORT || "8082";
---
+++ src/service/img.ts
@@ -0,0 +1,8 @@
+/**
+ * 处理用户通过websocket进入
+ */
+
+//进入
+export function UserJoin(id: string) {}
+//离开
+export function UserLeave(id: string) {}
